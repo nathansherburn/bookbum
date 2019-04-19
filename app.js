e78c459ba8ce76058352d7bc0811d0a4ad30abf8
Nathan Sherburn <nathansherburn@gmail.com>
2019-04-19 15:33:55 +1000

    Format with prettier js

--- app.js
+++ app.js
@@ -1,71 +1,82 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const AWS = require('aws-sdk')
-const express = require('express')
-const session = require('express-session')
-const LokiStore = require('connect-loki')(session)
-const bodyParser = require('body-parser')
+const AWS = require('aws-sdk');
+const express = require('express');
+const session = require('express-session');
+const LokiStore = require('connect-loki')(session);
+const bodyParser = require('body-parser');
 
-let app, s3
+let app, s3;
 
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_KEY
-})
+});
 
-app = express()
-s3 = new AWS.S3()
+app = express();
+s3 = new AWS.S3();
 
-app.use(express.static(__dirname + '/public'))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(session({
-  store: new LokiStore({ ttl: 0 }),
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true
-}))
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    store: new LokiStore({ ttl: 0 }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+  })
+);
 
-var auth = function (req, res, next) {
-  if (req.session && req.session.isAuthenticated) { return next() }
-  else { return res.redirect('/login.html') }
-}
+var auth = function(req, res, next) {
+  if (req.session && req.session.isAuthenticated) {
+    return next();
+  } else {
+    return res.redirect('/login.html');
+  }
+};
 
-app.post('/login', function (req, res) {
-  console.log(req.body)
-  let userMatch = req.body.username === 'admin'
-  let passwordMatch = req.body.password === process.env.PASSWORD
+app.post('/login', function(req, res) {
+  console.log(req.body);
+  let userMatch = req.body.username === 'admin';
+  let passwordMatch = req.body.password === process.env.PASSWORD;
   if (userMatch && passwordMatch) {
-    req.session.isAuthenticated = true
-    res.redirect('/')
+    req.session.isAuthenticated = true;
+    res.redirect('/');
+  } else {
+    res.redirect('/login.html');
   }
-  else {
-    res.redirect('/login.html')
-  }
-})
+});
 
-app.get('/logout', function (req, res) {
-  req.session.destroy()
-  res.send('goodbye')
-})
+app.get('/logout', function(req, res) {
+  req.session.destroy();
+  res.send('goodbye');
+});
 
-app.get('/api/v1/list-books', auth, function (req, res, next) {
-  s3.listObjects({
-    Bucket: 'bookbum'
-  }, function (err, data) {
-    if (err) next()
-    res.send(data.Contents)
-  })
-})
+app.get('/api/v1/list-books', auth, function(req, res, next) {
+  s3.listObjects(
+    {
+      Bucket: 'bookbum'
+    },
+    function(err, data) {
+      if (err) next();
+      res.send(data.Contents);
+    }
+  );
+});
 
-app.get('/api/v1/book/:name', auth, function (req, res, next) {
-  s3.getSignedUrl('getObject', {
-    Bucket: 'bookbum',
-    Key: req.params.name,
-    Expires: 60 * 5
-  }, function (err, url) {
-    if (err) next()
-    res.send({ url })
-  })
-})
+app.get('/api/v1/book/:name', auth, function(req, res, next) {
+  s3.getSignedUrl(
+    'getObject',
+    {
+      Bucket: 'bookbum',
+      Key: req.params.name,
+      Expires: 60 * 5
+    },
+    function(err, url) {
+      if (err) next();
+      res.send({ url });
+    }
+  );
+});
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT);
--- public/script.js
+++ public/script.js
@@ -1,63 +1,63 @@
-const audioEl = document.createElement('audio')
+const audioEl = document.createElement('audio');
 
-const player = document.getElementById('player')
-const totalTimeEl = document.getElementById('total-time')
-const currentTimeEl = document.getElementById('current-time')
-const sliderEl = document.getElementById("slider")
-const progressBarEl = document.getElementById('progress-bar')
-const togglePlayEl = document.getElementById("toggle-play")
-const bookListEl = document.getElementById('book-list')
-const playbackRateEl = document.getElementById('playback-rate')
-const bookInfoEl = document.getElementById('book-info')
-const sleepTimerEl = document.getElementById('sleep-timer')
+const player = document.getElementById('player');
+const totalTimeEl = document.getElementById('total-time');
+const currentTimeEl = document.getElementById('current-time');
+const sliderEl = document.getElementById('slider');
+const progressBarEl = document.getElementById('progress-bar');
+const togglePlayEl = document.getElementById('toggle-play');
+const bookListEl = document.getElementById('book-list');
+const playbackRateEl = document.getElementById('playback-rate');
+const bookInfoEl = document.getElementById('book-info');
+const sleepTimerEl = document.getElementById('sleep-timer');
 
-let previousSeekValue
-let seekMagnitude = 0
-let sleepTimer
-let fadeOutInterval
-let currentBook
-let progressCheckInterval
+let previousSeekValue;
+let seekMagnitude = 0;
+let sleepTimer;
+let fadeOutInterval;
+let currentBook;
+let progressCheckInterval;
 
 const bookbum = {
-  get: function (resource) {
+  get: function(resource) {
     return fetch('/api/v1/' + resource)
-      .then(function (response) {
-        return response.json()
+      .then(function(response) {
+        return response.json();
       })
-      .catch(function (err) {
-        console.error(err)
-        goToLogin()
-      })
+      .catch(function(err) {
+        console.error(err);
+        goToLogin();
+      });
   }
-}
+};
 
-init()
+init();
 
 function init() {
-  listTitles()
-  retrieveCurrentBook()
-  audioEl.ontimeupdate = updateTime
+  listTitles();
+  retrieveCurrentBook();
+  audioEl.ontimeupdate = updateTime;
 }
 
 function goToLogin() {
-  window.location.replace('/login.html')
+  window.location.replace('/login.html');
 }
 
 function retrieveCurrentBook() {
-  currentBook = localStorage.getItem('currentBook')
+  currentBook = localStorage.getItem('currentBook');
   if (currentBook) {
-    getBookUrl(currentBook).then(updateTime)
+    getBookUrl(currentBook).then(updateTime);
   }
 }
 
 function startProgressCheckInterval() {
-  let previousTime
-  progressCheckInterval = setInterval(function () {
+  let previousTime;
+  progressCheckInterval = setInterval(function() {
     if (audioEl.currentTime > previousTime) {
-      updateBookSettings()
+      updateBookSettings();
     }
-    previousTime = audioEl.currentTime
-  }, 3000)
+    previousTime = audioEl.currentTime;
+  }, 3000);
 }
 
 function updateBookSettings() {
@@ -66,150 +66,138 @@ function updateBookSettings() {
     currentTime: audioEl.currentTime,
     playbackRate: audioEl.playbackRate,
     volume: audioEl.volume
-  }
-  localStorage.setItem(bookInfoEl.innerText, JSON.stringify(settings))
+  };
+  localStorage.setItem(bookInfoEl.innerText, JSON.stringify(settings));
 }
 
 function seek(event) {
   if (previousSeekValue) {
-    seekMagnitude += event.touches[0].clientX - previousSeekValue
-    let newTime = audioEl.currentTime + seekMagnitude * 10
-    progressBarEl.style.width = 100 * newTime / audioEl.duration + '%'
+    seekMagnitude += event.touches[0].clientX - previousSeekValue;
+    let newTime = audioEl.currentTime + seekMagnitude * 10;
+    progressBarEl.style.width = (100 * newTime) / audioEl.duration + '%';
     currentTimeEl.innerText = new Date(newTime * 1000)
       .toISOString()
-      .substr(11, 8)
-    previousSeekValue = event.touches[0].clientX
-  }
-  else {
-    seekMagnitude = 0
-    previousSeekValue = event.touches[0].clientX
-    audioEl.ontimeupdate = function () { }
+      .substr(11, 8);
+    previousSeekValue = event.touches[0].clientX;
+  } else {
+    seekMagnitude = 0;
+    previousSeekValue = event.touches[0].clientX;
+    audioEl.ontimeupdate = function() {};
   }
 }
 
 function stopSeek(event) {
-  audioEl.currentTime = audioEl.currentTime + seekMagnitude * 10
-  audioEl.ontimeupdate = updateTime
-  previousSeekValue = null
-  updateBookSettings()
+  audioEl.currentTime = audioEl.currentTime + seekMagnitude * 10;
+  audioEl.ontimeupdate = updateTime;
+  previousSeekValue = null;
+  updateBookSettings();
 }
 
 function updateTime() {
-  let percentageProgress = 100 * audioEl.currentTime / audioEl.duration
-  progressBarEl.style.width = percentageProgress + '%'
+  let percentageProgress = (100 * audioEl.currentTime) / audioEl.duration;
+  progressBarEl.style.width = percentageProgress + '%';
   currentTimeEl.innerText = new Date(audioEl.currentTime * 1000)
     .toISOString()
-    .substr(11, 8)
+    .substr(11, 8);
 }
 
 function listTitles(books) {
-  bookbum.get('list-books')
-    .then(function (books) {
-      books.forEach(function (book) {
-        let div = document.createElement('div')
-        div.classList.add('book')
-        div.innerText = book.Key
-        div.onclick = function (event) {
-          getBookUrl(event.target.innerText).then(play)
-        }
-        bookListEl.appendChild(div)
-      })
-    })
+  bookbum.get('list-books').then(function(books) {
+    books.forEach(function(book) {
+      let div = document.createElement('div');
+      div.classList.add('book');
+      div.innerText = book.Key;
+      div.onclick = function(event) {
+        getBookUrl(event.target.innerText).then(play);
+      };
+      bookListEl.appendChild(div);
+    });
+  });
 }
 
 function getBookUrl(book) {
-  localStorage.setItem('currentBook', book)
-  bookInfoEl.innerText = book
-  return bookbum.get('book/' + book)
-    .then(function (url) {
-      audioEl.src = url.url
-      audioEl.onloadedmetadata = function () {
-        let bookSettings = JSON.parse(localStorage.getItem(book))
-        if (bookSettings) {
-          audioEl.currentTime = bookSettings.currentTime || 0.0
-          audioEl.volume = bookSettings.volume || 1.0
-          audioEl.playbackRate = bookSettings.playbackRate || 1.0
-        }
-        updateTotalTime()
-        updateTime()
-        playbackRateEl.innerText = 'X' + audioEl.playbackRate.toFixed(2)
+  localStorage.setItem('currentBook', book);
+  bookInfoEl.innerText = book;
+  return bookbum.get('book/' + book).then(function(url) {
+    audioEl.src = url.url;
+    audioEl.onloadedmetadata = function() {
+      let bookSettings = JSON.parse(localStorage.getItem(book));
+      if (bookSettings) {
+        audioEl.currentTime = bookSettings.currentTime || 0.0;
+        audioEl.volume = bookSettings.volume || 1.0;
+        audioEl.playbackRate = bookSettings.playbackRate || 1.0;
       }
-    })
+      updateTotalTime();
+      updateTime();
+      playbackRateEl.innerText = 'X' + audioEl.playbackRate.toFixed(2);
+    };
+  });
 }
 
 function updateTotalTime() {
   totalTimeEl.innerText = new Date(audioEl.duration * 1000)
     .toISOString()
-    .substr(11, 8)
-}
-
-
-function canPlayBook() {
-  return audioEl !== null
-    && audioEl.canPlayType
-    && audioEl.canPlayType("audio/mpeg")
+    .substr(11, 8);
 }
 
 function togglePlay() {
-  audioEl.paused ? play() : pause()
+  audioEl.paused ? play() : pause();
 }
 
 function play() {
-  audioEl.volume = 1.0
-  audioEl.play()
-  togglePlayEl.innerText = 'PAUSE'
-  startProgressCheckInterval()
+  audioEl.volume = 1.0;
+  audioEl.play();
+  togglePlayEl.innerText = 'PAUSE';
+  startProgressCheckInterval();
 }
 
 function pause() {
-  audioEl.pause()
-  togglePlayEl.innerText = 'PLAY'
-  clearInterval(progressCheckInterval)
+  audioEl.pause();
+  togglePlayEl.innerText = 'PLAY';
+  clearInterval(progressCheckInterval);
 }
 
 function setVolume(volume) {
-  audioEl.volume = volume
-  updateBookSettings()
+  audioEl.volume = volume;
+  updateBookSettings();
 }
 
 function changePlaybackRate() {
-  audioEl.playbackRate = (audioEl.playbackRate > 1.75)
-    ? 1
-    : audioEl.playbackRate + 0.25
-  playbackRateEl.innerText = 'X' + audioEl.playbackRate.toFixed(2)
-  updateBookSettings()
+  audioEl.playbackRate =
+    audioEl.playbackRate > 1.75 ? 1 : audioEl.playbackRate + 0.25;
+  playbackRateEl.innerText = 'X' + audioEl.playbackRate.toFixed(2);
+  updateBookSettings();
 }
 
 function toggleSleepTimer() {
   if (sleepTimer) {
-    clearTimeout(sleepTimer)
-    clearInterval(fadeOutInterval)
-    sleepTimerEl.classList.remove('selected')
-    audioEl.volume = 1.0
-    sleepTimer = false
-  }
-  else {
-    sleepTimer = setTimeout(fadeOut, 30 * 60 * 1000)
-    sleepTimerEl.classList.add('selected')
+    clearTimeout(sleepTimer);
+    clearInterval(fadeOutInterval);
+    sleepTimerEl.classList.remove('selected');
+    audioEl.volume = 1.0;
+    sleepTimer = false;
+  } else {
+    sleepTimer = setTimeout(fadeOut, 30 * 60 * 1000);
+    sleepTimerEl.classList.add('selected');
   }
 }
 
 function fadeOut() {
-  fadeOutInterval = setInterval(function () {
+  fadeOutInterval = setInterval(function() {
     if (audioEl.volume <= 0.1) {
-      audioEl.pause()
-      togglePlayEl.innerText = 'PLAY'
-      clearInterval(fadeInterval)
-      return
+      audioEl.pause();
+      togglePlayEl.innerText = 'PLAY';
+      clearInterval(fadeInterval);
+      return;
     }
-    audioEl.volume -= 0.1
-  }, 2000)
+    audioEl.volume -= 0.1;
+  }, 2000);
 }
 
 function skipBackward() {
-  audioEl.currentTime = audioEl.currentTime - 10
+  audioEl.currentTime = audioEl.currentTime - 10;
 }
 
 function skipForward() {
-  audioEl.currentTime = audioEl.currentTime + 10
+  audioEl.currentTime = audioEl.currentTime + 10;
 }
